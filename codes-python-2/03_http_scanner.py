5cb8bd58c394a2de9a5e521752728cca352eef65
henri-kapidani <h.kapidani@gmail.com>
2025-12-04 07:02:15 -0500

    python 4

---
+++ codes-python-2/03_http_scanner.py
@@ -0,0 +1,24 @@
+import http.client
+
+host = input('Inserire IP: ')
+port = input('Inserire la porta (default: 80): ')
+
+if port == '':
+    port = 80
+else:
+    port = int(port)
+
+try:
+    connection = http.client.HTTPConnection(host, port)
+    connection.request('GET', '/')
+    response = connection.getresponse()
+    print(response)
+    print('status:', response.status)
+
+    body_bytes = response.read()
+    body_string = body_bytes.decode('utf-8')
+    print(body_string)
+
+    connection.close()
+except ConnectionRefusedError:
+    print("Connessione fallita")
